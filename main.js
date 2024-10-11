39c99b9cbe791af89368101851259e4e06ff40c4
Bernardo Carneiro <bernardocm1089@gmail.com>
2024-10-11 16:53:53 -0300

    local storage added

--- main.js
+++ main.js
@@ -1,15 +1,20 @@
 const emailform = document.querySelector(".emailform");
 const emailinsert = document.querySelector(".uemailinsert");
+const passwordinsert = document.querySelector(".unameinsert");
 const resultmessage = document.querySelector(".resultMessage");
 
 emailform.addEventListener("submit", function (event){
     event.preventDefault();
 
     const email = emailinsert.value;
+    const password = passwordinsert.value;
 
-    const isValid = validateEmail(email)
+    const isValid = validateEmail(email, password);
 
     if(isValid){
+        localStorage.setItem('email', email);
+        localStorage.setItem('password', password);
+        
         resultmessage.textContent = "Cadastro concluído! Já estamos te redirecionando."
         resultmessage.style.color = "black";
         setTimeout(function() {
